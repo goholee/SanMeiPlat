75c76b8768683dd177b50a6bf3fe7172bf334b2f
goho <goholee@126.com>
2019-03-05 22:00:04 +0800

    创建课程及课程类型实体

---
+++ src/SanMeiPlat.Core/CourseTypes/CourseTypes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using System.ComponentModel.DataAnnotations;
+
+namespace SanMeiPlat.CourseTypes
+{
+    /// <summary>
+    /// 课程种类
+    /// </summary>
+    public class CourseTypes:FullAuditedEntity
+    {
+        /// <summary>
+        /// 课程种类名称
+        /// </summary>
+        [Required]
+        [MaxLength(50)]
+        public string CourseTypeName { get; set; }
+    }
+}
---
+++ src/SanMeiPlat.Core/Courses/Courses.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Abp.Domain.Entities.Auditing;
+using System.ComponentModel.DataAnnotations;
+
+namespace SanMeiPlat.Courses
+{
+    public class Courses:FullAuditedEntity
+    {
+        /// <summary>
+        /// 课程类型
+        /// </summary>
+        [Required]
+        public CourseTypes.CourseTypes CourseType { get; set; }
+
+        /// <summary>
+        /// 课程期数
+        /// </summary>
+        public int CourseNum { get; set; }
+
+        /// <summary>
+        /// 课程名称
+        /// </summary>
+        [MaxLength(200)]
+        public string CourseName { get; set; }
+
+        /// <summary>
+        /// 授课讲师
+        /// </summary>
+        [MaxLength(20)]
+        public string CourseTeacher { get; set; }
+
+        /// <summary>
+        /// 授课地点
+        /// </summary>
+        [MaxLength(500)]
+        public string CourseAddress { get; set; }
+
+        /// <summary>
+        /// 开课时间
+        /// </summary>
+        [MaxLength(50)]
+        public string CourseStartDate { get; set; }
+
+        /// <summary>
+        /// 课程报名老师，课程联系人
+        /// </summary>
+        [MaxLength(20)]
+        public string CourseContact { get; set; }
+
+        /// <summary>
+        /// 课程报名电话，课程联系人电话
+        /// </summary>
+        [MaxLength(11)]
+        public string CourseContactNumber { get; set; }
+
+
+
+    }
+}
